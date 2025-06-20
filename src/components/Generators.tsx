7fa305c0f169040c9a9a2c972b17f8d4df64b8f4
Adam Lay <adam93.lay+git@gmail.com>
2025-06-20 14:50:31 +0100

    Gen order

--- src/components/Generators.tsx
+++ src/components/Generators.tsx
@@ -1,45 +1,14 @@
-import { useShallow } from "zustand/shallow";
-import { GEN_MAX_LEVEL, generators } from "../data/generators";
-import { useStore } from "../store/store";
+import { generators } from "../data/generators";
 import BuyCountSelector from "./BuyCountSelector";
 import GeneratorListItem from "./GeneratorListItem";
-import { orderBy } from "lodash";
 
 export default function Generators() {
-  const [myGenerators] = useStore(useShallow((state) => [state.generators]));
-  const indexMap = generators.reduce(
-    (acc, gen, i) => ({
-      ...acc,
-      [gen.name]: i,
-    }),
-    {} as any
-  );
-  const myMaxGenerators = myGenerators.filter((gen) => gen.level >= GEN_MAX_LEVEL);
-  const myCurrentGenerators = myGenerators.filter((gen) => gen.level < GEN_MAX_LEVEL);
-  const availableGenerators = generators.filter(
-    (x) => !myGenerators.some((my) => my.name === x.name)
-  );
-
   return (
     <div>
       <BuyCountSelector />
       <ul className="list bg-base-100 rounded-box shadow-md mt-2">
-        {orderBy(myCurrentGenerators, (x) => indexMap[x.name]).map((generator) => (
-          <GeneratorListItem
-            key={generator.name}
-            name={generator.name}
-            definition={generators.find((x) => x.name === generator.name)!}
-          />
-        ))}
-        {availableGenerators.map((generator) => (
+        {generators.map((generator) => (
           <GeneratorListItem key={generator.name} name={generator.name} definition={generator} />
-        ))}
-        {myMaxGenerators.map((generator) => (
-          <GeneratorListItem
-            key={generator.name}
-            name={generator.name}
-            definition={generators.find((x) => x.name === generator.name)!}
-          />
         ))}
       </ul>
     </div>
